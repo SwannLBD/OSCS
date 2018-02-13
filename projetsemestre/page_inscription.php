bb10029917a0e396e452baa629575d0db57d636e
alex <alexpouivet@hotmail.fr>
2018-02-13 10:16:46 +0100

    changement css page inscription Alex

--- projetsemestre/page_inscription.php
+++ projetsemestre/page_inscription.php
@@ -37,17 +37,11 @@
 	</div>
 	</div>
 	</div>	
-</main>
-<footer class="block-footer">
-					<div class="info1">
-						<p>Mentions légales</p>
-					</div>
-					<div class="info2">
-						<p>Politique de confidentialité</p>
-					</div>	
-					<div class="info3">
-						<p>Conditions générales</p> 
-					</div>
-</footer>	
+</main>	
+<?php
+	
+include "include/footer.php";
+	
+?>	
 </body>
 </html>
